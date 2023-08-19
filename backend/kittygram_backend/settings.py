1d6b901706c0127914d6fdb36eef42b27b602ff8
saturnsergei <rings.of.saturn@yandex.ru>
2023-08-19 20:49:05 +0000

    fix review

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -7,10 +7,11 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY', "")
+SECRET_KEY = os.getenv('SECRET_KEY', '')
 DEBUG = os.getenv('DEBUG', False)
 
-ALLOWED_HOSTS = ['158.160.75.183', '127.0.0.1', 'localhost', 'kittyonsaturn.ddns.net']
+ALLOWED_HOSTS_ENV = os.getenv('ALLOWED_HOSTS')
+ALLOWED_HOSTS = ALLOWED_HOSTS_ENV.split() if ALLOWED_HOSTS_ENV else []
 
 INSTALLED_APPS = [
     'django.contrib.admin',
